8f4e1c2506da1a10447a0f8e0132dd6c6552e082
aliffiazmuhammed <alifmgdi@gmail.com>
2023-04-10 00:58:36 +0530

    array of list and post method updated

--- app.js
+++ app.js
@@ -9,9 +9,14 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-
+const todo = []
 app.get('/',(req,res)=>{
-    res.render('index');
+    res.render('index',{todo:todo});
+})
+
+app.post('/',(req,res)=>{
+    todo.push(req.body.newItem)
+    res.redirect('/')
 })
 
 app.listen(3000,()=>{
